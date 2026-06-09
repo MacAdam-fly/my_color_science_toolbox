10ffb57c81830be5d79b62c98b5df7a821315451
MacAdam-fly <xioayf@mail.ustc.edu.cn>
2026-06-09 20:41:54 +0800

    照明常数XYZ添加只读

--- color/constants/illuminants_XYZ.py
+++ color/constants/illuminants_XYZ.py
@@ -4,9 +4,16 @@ from __future__ import annotations
 
 import numpy as np
 
-A_XYZ = np.array([109.8490612345073, 100.0, 35.579825745490254], dtype=float)
-C_XYZ = np.array([98.0705971659919, 100.0, 118.22494939271255], dtype=float)
-D50_XYZ = np.array([96.4212, 100.0, 82.5188], dtype=float)
-D55_XYZ = np.array([95.682, 100.0, 92.149], dtype=float)
-D65_XYZ = np.array([95.047, 100.0, 108.883], dtype=float)
-E_XYZ = np.array([100.0, 100.0, 100.0], dtype=float)
+
+def _readonly_vector(values: list[float]) -> np.ndarray:
+	"""Return a read-only XYZ vector."""
+	vector = np.array(values, dtype=np.float64)
+	vector.setflags(write=False)
+	return vector
+
+A_XYZ = _readonly_vector([109.8490612345073, 100.0, 35.579825745490254])
+C_XYZ = _readonly_vector([98.0705971659919, 100.0, 118.22494939271255])
+D50_XYZ = _readonly_vector([96.4212, 100.0, 82.5188])
+D55_XYZ = _readonly_vector([95.682, 100.0, 92.149])
+D65_XYZ = _readonly_vector([95.047, 100.0, 108.883])
+E_XYZ = _readonly_vector([100.0, 100.0, 100.0])
